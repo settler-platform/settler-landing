b9afeb0b43cb339e9d7b0d3acc4e148259069976
Elshad Seyidmammadov <elshad@eclipsesource.com>
2023-03-08 10:53:54 +0400

    🐛 Improve main module form

--- main.js
+++ main.js
@@ -36,6 +36,7 @@ const input = document.querySelector("#input");
 const iti = window.intlTelInput(input, {
     initialCountry: "ae",
     onlyCountries: ["ae"],
+    formatOnDisplay: false,
     utilsScript:
         "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/utils.js",
     customPlaceholder: function (_, selectedCountryData) {
@@ -43,7 +44,9 @@ const iti = window.intlTelInput(input, {
     },
 });
 $("#input").inputmask("+999 999 999 9999", {
-    placeholder: "_", showMaskOnHover: false, greedy: false
+    placeholder: "_",
+    showMaskOnHover: false,
+    greedy: false
 });
 
 $(function () {
@@ -191,25 +194,7 @@ $(function () {
     $(".button_on_page , .modal_form_close , .menu_button_2_home").on("click", function (e) {
         $("body").toggleClass("hidden_body");
     });
-    $(".button_on_page").on("click", function (e) {
-        var form = $('form[data-name="Contact Form"]');
-        var submitButton = form.find("input[type=submit]");
-        const nameInput = form.find("input[type=text]");
-        const phoneInput = form.find("input[type=tel]");
-        submitButton.attr("disabled", true);
-        var termsCheckbox = form.find($("#checkbox"));
-        $(form).on('change', 'input', function () {
-            $(input).on('input', 'propertychange', 'change', function (e) {
-                const isValid = iti.isValidNumber();
-                const error = iti.getValidationError();
-            });
-            if ($(termsCheckbox).is(":checked") & nameInput.val().length > 1 & iti.isValidNumber() === true) {
-                submitButton.attr("disabled", false);
-            } else {
-                submitButton.attr("disabled", true);
-            }
-        });
-    });
+
     function convertFormToJSON(r) { var e = $(r).serializeArray(), n = {}; return $.each(e, function () { n[this.name] = this.value || "" }), n }
     makeWebflowFormAjax = function (forms, successCallback, errorCallback) {
         forms.each(function () {
@@ -282,3 +267,39 @@ $(".main_stories").each(function (index) {
     return landingScrollTimeline;
 });
 $(".text-field-2.w-input").attr("maxlength", "17");
+
+$(".button_on_page").on("click", validateForm);
+
+function registerMainModuleListeners() {
+    const { nameInput, phoneInput, termsCheckbox } = getFormFields();
+    nameInput.on('input', validateForm);
+    phoneInput.on('input', validateForm);
+    termsCheckbox.on('change', validateForm);
+}
+
+function validateForm() {
+    const { submitButton } = getFormFields();
+    submitButton.attr("disabled", !checkFormValidation());
+}
+
+function checkFormValidation() {
+    const { nameInput, termsCheckbox } = getFormFields();
+    const isValidName = nameInput.val().trim().length > 1;
+    const numberValidationError = iti.getValidationError();
+    const isValidNumber = iti.isValidNumber() ||
+        (numberValidationError === 0 || numberValidationError === 4);
+    const isTermsChecked = $(termsCheckbox).is(":checked");
+    return isValidName & isValidNumber & isTermsChecked;
+}
+
+function getFormFields() {
+    var form = $('form[data-name="Contact Form"]');
+    return {
+        nameInput: form.find("input[type=text]"),
+        phoneInput: form.find("input[type=tel]"),
+        termsCheckbox: form.find("#checkbox"),
+        submitButton: form.find("input[type=submit]")
+    };
+}
+
+registerMainModuleListeners();
